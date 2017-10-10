50f06bad216db1ba66349d7d2b338e863f988710
naivezm <jack_wang_jie@163.com>
2017-10-10 21:36:02 +0800

    sql alter

--- WindowsCanToolApp/WindowsCanToolApp/Properties/Settings.Designer.cs
+++ WindowsCanToolApp/WindowsCanToolApp/Properties/Settings.Designer.cs
@@ -22,5 +22,15 @@ namespace WindowsCanToolApp.Properties {
                 return defaultInstance;
             }
         }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=.;Initial Catalog=CanTool;Integrated Security=True")]
+        public string CanToolConnectionString {
+            get {
+                return ((string)(this["CanToolConnectionString"]));
+            }
+        }
     }
 }
