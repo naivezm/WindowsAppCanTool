7a4f7d8ad67babc4f9938a70a53f59192710bdef
Zds501710271 <15522032518@163.com>
2017-10-11 17:10:15 +0800

    SendReceiveAlter

--- WindowsCanToolApp/WindowsCanToolApp/Form1.Designer.cs
+++ WindowsCanToolApp/WindowsCanToolApp/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.基本设置ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.cOM口设置ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
@@ -41,16 +42,20 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.Mainpanel = new System.Windows.Forms.Panel();
+            this.SendReceivepanel = new System.Windows.Forms.Panel();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SendTextBox = new System.Windows.Forms.TextBox();
+            this.ReceiveTextBox = new System.Windows.Forms.TextBox();
             this.COMpanel = new System.Windows.Forms.Panel();
             this.button3 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.comboBox6 = new System.Windows.Forms.ComboBox();
-            this.comboBox5 = new System.Windows.Forms.ComboBox();
-            this.comboBox4 = new System.Windows.Forms.ComboBox();
-            this.comboBox3 = new System.Windows.Forms.ComboBox();
-            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.StopBitscomboBox = new System.Windows.Forms.ComboBox();
+            this.DataBitscomboBox = new System.Windows.Forms.ComboBox();
+            this.ParityErrorCharcomboBox = new System.Windows.Forms.ComboBox();
+            this.ParitycomboBox = new System.Windows.Forms.ComboBox();
+            this.BaudRatecomboBox = new System.Windows.Forms.ComboBox();
             this.label7 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
@@ -59,10 +64,12 @@
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.Explainlabel2 = new System.Windows.Forms.Label();
             this.Explainlabel1 = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.COMcomboBox = new System.Windows.Forms.ComboBox();
             this.SendAndRecvlabel = new System.Windows.Forms.Label();
+            this.sPort = new System.IO.Ports.SerialPort(this.components);
             this.menuStrip1.SuspendLayout();
             this.Mainpanel.SuspendLayout();
+            this.SendReceivepanel.SuspendLayout();
             this.COMpanel.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.groupBox1.SuspendLayout();
@@ -137,6 +144,7 @@
             this.cAN信息发送ToolStripMenuItem.Name = "cAN信息发送ToolStripMenuItem";
             this.cAN信息发送ToolStripMenuItem.Size = new System.Drawing.Size(94, 21);
             this.cAN信息发送ToolStripMenuItem.Text = "CAN信息发送";
+            this.cAN信息发送ToolStripMenuItem.Click += new System.EventHandler(this.cAN信息发送ToolStripMenuItem_Click);
             // 
             // panel2
             // 
@@ -161,12 +169,48 @@
             // 
             // Mainpanel
             // 
-            //this.Mainpanel.Controls.Add(this.COMpanel);
+            //this.Mainpanel.Controls.Add(this.SendReceivepanel);
             this.Mainpanel.Location = new System.Drawing.Point(0, 29);
             this.Mainpanel.Name = "Mainpanel";
             this.Mainpanel.Size = new System.Drawing.Size(563, 332);
             this.Mainpanel.TabIndex = 1;
             this.Mainpanel.Paint += new System.Windows.Forms.PaintEventHandler(this.Mainpanel_Paint);
+            // 
+            // SendReceivepanel
+            // 
+            this.SendReceivepanel.Controls.Add(this.button4);
+            this.SendReceivepanel.Controls.Add(this.SendTextBox);
+            this.SendReceivepanel.Controls.Add(this.ReceiveTextBox);
+            this.SendReceivepanel.Location = new System.Drawing.Point(3, 0);
+            this.SendReceivepanel.Name = "SendReceivepanel";
+            this.SendReceivepanel.Size = new System.Drawing.Size(557, 300);
+            this.SendReceivepanel.TabIndex = 0;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(488, 147);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(60, 114);
+            this.button4.TabIndex = 2;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // SendTextBox
+            // 
+            this.SendTextBox.Location = new System.Drawing.Point(215, 147);
+            this.SendTextBox.Multiline = true;
+            this.SendTextBox.Name = "SendTextBox";
+            this.SendTextBox.Size = new System.Drawing.Size(267, 114);
+            this.SendTextBox.TabIndex = 1;
+            // 
+            // ReceiveTextBox
+            // 
+            this.ReceiveTextBox.Location = new System.Drawing.Point(215, 3);
+            this.ReceiveTextBox.Multiline = true;
+            this.ReceiveTextBox.Name = "ReceiveTextBox";
+            this.ReceiveTextBox.Size = new System.Drawing.Size(333, 136);
+            this.ReceiveTextBox.TabIndex = 0;
             // 
             // COMpanel
             // 
@@ -197,6 +241,7 @@
             this.button2.TabIndex = 3;
             this.button2.Text = "Cancel";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button1
             // 
@@ -206,14 +251,15 @@
             this.button1.TabIndex = 2;
             this.button1.Text = "Ok";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // groupBox2
             // 
-            this.groupBox2.Controls.Add(this.comboBox6);
-            this.groupBox2.Controls.Add(this.comboBox5);
-            this.groupBox2.Controls.Add(this.comboBox4);
-            this.groupBox2.Controls.Add(this.comboBox3);
-            this.groupBox2.Controls.Add(this.comboBox2);
+            this.groupBox2.Controls.Add(this.StopBitscomboBox);
+            this.groupBox2.Controls.Add(this.DataBitscomboBox);
+            this.groupBox2.Controls.Add(this.ParityErrorCharcomboBox);
+            this.groupBox2.Controls.Add(this.ParitycomboBox);
+            this.groupBox2.Controls.Add(this.BaudRatecomboBox);
             this.groupBox2.Controls.Add(this.label7);
             this.groupBox2.Controls.Add(this.label6);
             this.groupBox2.Controls.Add(this.label5);
@@ -226,61 +272,72 @@
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "COM port Settings.";
             // 
-            // comboBox6
+            // StopBitscomboBox
             // 
-            this.comboBox6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
-            this.comboBox6.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.comboBox6.FormattingEnabled = true;
-            this.comboBox6.Items.AddRange(new object[] {
+            this.StopBitscomboBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.StopBitscomboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.StopBitscomboBox.FormattingEnabled = true;
+            this.StopBitscomboBox.Items.AddRange(new object[] {
             "1"});
-            this.comboBox6.Location = new System.Drawing.Point(392, 72);
-            this.comboBox6.Name = "comboBox6";
-            this.comboBox6.Size = new System.Drawing.Size(121, 20);
-            this.comboBox6.TabIndex = 9;
-            this.comboBox6.SelectedIndexChanged += new System.EventHandler(this.comboBox6_SelectedIndexChanged);
+            this.StopBitscomboBox.Location = new System.Drawing.Point(392, 72);
+            this.StopBitscomboBox.Name = "StopBitscomboBox";
+            this.StopBitscomboBox.Size = new System.Drawing.Size(121, 20);
+            this.StopBitscomboBox.TabIndex = 9;
+            this.StopBitscomboBox.SelectedIndexChanged += new System.EventHandler(this.comboBox6_SelectedIndexChanged);
             // 
-            // comboBox5
+            // DataBitscomboBox
             // 
-            this.comboBox5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
-            this.comboBox5.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.comboBox5.FormattingEnabled = true;
-            this.comboBox5.Items.AddRange(new object[] {
+            this.DataBitscomboBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.DataBitscomboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.DataBitscomboBox.FormattingEnabled = true;
+            this.DataBitscomboBox.Items.AddRange(new object[] {
             "8"});
-            this.comboBox5.Location = new System.Drawing.Point(392, 34);
-            this.comboBox5.Name = "comboBox5";
-            this.comboBox5.Size = new System.Drawing.Size(121, 20);
-            this.comboBox5.TabIndex = 8;
+            this.DataBitscomboBox.Location = new System.Drawing.Point(392, 34);
+            this.DataBitscomboBox.Name = "DataBitscomboBox";
+            this.DataBitscomboBox.Size = new System.Drawing.Size(121, 20);
+            this.DataBitscomboBox.TabIndex = 8;
             // 
-            // comboBox4
+            // ParityErrorCharcomboBox
             // 
-            this.comboBox4.FormattingEnabled = true;
-            this.comboBox4.Location = new System.Drawing.Point(148, 107);
-            this.comboBox4.Name = "comboBox4";
-            this.comboBox4.Size = new System.Drawing.Size(121, 20);
-            this.comboBox4.TabIndex = 7;
+            this.ParityErrorCharcomboBox.FormattingEnabled = true;
+            this.ParityErrorCharcomboBox.Location = new System.Drawing.Point(148, 107);
+            this.ParityErrorCharcomboBox.Name = "ParityErrorCharcomboBox";
+            this.ParityErrorCharcomboBox.Size = new System.Drawing.Size(121, 20);
+            this.ParityErrorCharcomboBox.TabIndex = 7;
             // 
-            // comboBox3
+            // ParitycomboBox
             // 
-            this.comboBox3.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.comboBox3.FormattingEnabled = true;
-            this.comboBox3.Items.AddRange(new object[] {
+            this.ParitycomboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.ParitycomboBox.FormattingEnabled = true;
+            this.ParitycomboBox.Items.AddRange(new object[] {
             "ODD",
             "EVEN",
             "MARK",
             "SPACE",
             "NONE"});
-            this.comboBox3.Location = new System.Drawing.Point(148, 72);
-            this.comboBox3.Name = "comboBox3";
-            this.comboBox3.Size = new System.Drawing.Size(121, 20);
-            this.comboBox3.TabIndex = 6;
+            this.ParitycomboBox.Location = new System.Drawing.Point(148, 72);
+            this.ParitycomboBox.Name = "ParitycomboBox";
+            this.ParitycomboBox.Size = new System.Drawing.Size(121, 20);
+            this.ParitycomboBox.TabIndex = 6;
             // 
-            // comboBox2
+            // BaudRatecomboBox
             // 
-            this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(148, 34);
-            this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(121, 20);
-            this.comboBox2.TabIndex = 5;
+            this.BaudRatecomboBox.FormattingEnabled = true;
+            this.BaudRatecomboBox.Items.AddRange(new object[] {
+            "1200",
+            "2400",
+            "4800",
+            "9600",
+            "19200",
+            "38400",
+            "43000",
+            "56000",
+            "57600",
+            "115200"});
+            this.BaudRatecomboBox.Location = new System.Drawing.Point(148, 34);
+            this.BaudRatecomboBox.Name = "BaudRatecomboBox";
+            this.BaudRatecomboBox.Size = new System.Drawing.Size(121, 20);
+            this.BaudRatecomboBox.TabIndex = 5;
             // 
             // label7
             // 
@@ -331,7 +388,7 @@
             // 
             this.groupBox1.Controls.Add(this.Explainlabel2);
             this.groupBox1.Controls.Add(this.Explainlabel1);
-            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.COMcomboBox);
             this.groupBox1.Controls.Add(this.SendAndRecvlabel);
             this.groupBox1.Location = new System.Drawing.Point(3, 3);
             this.groupBox1.Name = "groupBox1";
@@ -357,14 +414,14 @@
             this.Explainlabel1.TabIndex = 2;
             this.Explainlabel1.Text = "Choose a COM port (e.g.COM3) or specify a network";
             // 
-            // comboBox1
+            // COMcomboBox
             // 
-            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(16, 32);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(162, 20);
-            this.comboBox1.TabIndex = 1;
+            this.COMcomboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.COMcomboBox.FormattingEnabled = true;
+            this.COMcomboBox.Location = new System.Drawing.Point(16, 32);
+            this.COMcomboBox.Name = "COMcomboBox";
+            this.COMcomboBox.Size = new System.Drawing.Size(162, 20);
+            this.COMcomboBox.TabIndex = 1;
             // 
             // SendAndRecvlabel
             // 
@@ -374,6 +431,10 @@
             this.SendAndRecvlabel.Size = new System.Drawing.Size(137, 12);
             this.SendAndRecvlabel.TabIndex = 0;
             this.SendAndRecvlabel.Text = "Send/Recvieve on Comm.";
+            // 
+            // sPort
+            // 
+            this.sPort.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.sPort_DataReceived);
             // 
             // Form1
             // 
@@ -389,6 +450,8 @@
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             this.Mainpanel.ResumeLayout(false);
+            this.SendReceivepanel.ResumeLayout(false);
+            this.SendReceivepanel.PerformLayout();
             this.COMpanel.ResumeLayout(false);
             this.groupBox2.ResumeLayout(false);
             this.groupBox2.PerformLayout();
@@ -422,18 +485,23 @@
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Label Explainlabel2;
         private System.Windows.Forms.Label Explainlabel1;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox COMcomboBox;
         private System.Windows.Forms.Label SendAndRecvlabel;
-        private System.Windows.Forms.ComboBox comboBox6;
-        private System.Windows.Forms.ComboBox comboBox5;
-        private System.Windows.Forms.ComboBox comboBox4;
-        private System.Windows.Forms.ComboBox comboBox3;
-        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox StopBitscomboBox;
+        private System.Windows.Forms.ComboBox DataBitscomboBox;
+        private System.Windows.Forms.ComboBox ParityErrorCharcomboBox;
+        private System.Windows.Forms.ComboBox ParitycomboBox;
+        private System.Windows.Forms.ComboBox BaudRatecomboBox;
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel SendReceivepanel;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox SendTextBox;
+        private System.Windows.Forms.TextBox ReceiveTextBox;
+        private System.IO.Ports.SerialPort sPort;
     }
 }
 
--- WindowsCanToolApp/WindowsCanToolApp/Form1.cs
+++ WindowsCanToolApp/WindowsCanToolApp/Form1.cs
@@ -32,6 +32,7 @@ namespace WindowsCanToolApp
 
         private void cOM口设置ToolStripMenuItem_Click(object sender, EventArgs e)
         {
+            this.Mainpanel.Controls.Clear();
             this.Mainpanel.Controls.Add(this.COMpanel);
             GetCOMList();
         }
@@ -43,7 +44,11 @@ namespace WindowsCanToolApp
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            sPort.PortName = "COM2";
+            sPort.Open();
+            sPort.DataReceived += new SerialDataReceivedEventHandler(sPort_DataReceived);
+            //将事件处理函数，挂接到事件DataReceived 中
+           
         }
 
         private void Mainpanel_Paint(object sender, PaintEventArgs e)
@@ -67,14 +72,84 @@ namespace WindowsCanToolApp
             if (keyCOM != null)
             {
                 string[] sSubKeys = keyCOM.GetValueNames();
-                this.comboBox1.Items.Clear();
+                this.COMcomboBox.Items.Clear();
                 foreach (string sName in sSubKeys)
                 {
                     string sValue = (string)keyCOM.GetValue(sName);
-                    this.comboBox1.Items.Add(sValue);
+                    this.COMcomboBox.Items.Add(sValue);
                 }
             }
         
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Mainpanel.Controls.Clear();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void cAN信息发送ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Mainpanel.Controls.Clear();
+            this.Mainpanel.Controls.Add(this.SendReceivepanel);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                //byte[] data = Encoding.Unicode.GetBytes(textBox1.Text);
+                string data = SendTextBox.Text.ToString();
+                // string str = Convert.ToBase64String(data);
+                sPort.WriteLine(data);
+                // sPort.Close();
+                MessageBox.Show("数据发送成功！", "系统提示");
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message.ToString(), "提示信息");
+            }
+        }
+
+        private void sPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            byte[] ReDatas = new byte[sPort.BytesToRead];
+            sPort.Read(ReDatas, 0, ReDatas.Length);//读取数据
+            this.AddData(ReDatas);//输出数据
+        }
+         /// <summary>
+         /// 添加数据
+         /// </summary>
+         /// <param name="data">字节数组</param>
+         public void AddData(byte[] data)
+        {
+            AddContent(new ASCIIEncoding().GetString(data));
+            
+         }
+
+         /// <summary>
+         /// 输入到显示区域
+         /// </summary>
+        /// <param name="content"></param>
+         private void AddContent(string content)
+         {
+             this.BeginInvoke(new MethodInvoker(delegate//在创建控件的基础句柄所在线程上异步执行指定委托。
+             {
+                 if(ReceiveTextBox.Text.Length>0)//存在内容
+                 {
+                     ReceiveTextBox.AppendText("\r\n");//换行
+                 }
+                 ReceiveTextBox.AppendText(content);
+             }));
+         }
+      
     }
+    
 }
